5f150f2318fa9e15c4033e695143b52b2a084d1a
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2015-11-13 07:56:49 +0100

    Mars Lander level 2: WIP

--- src/mars_lander_lvl_2.cpp
+++ src/mars_lander_lvl_2.cpp
@@ -26,6 +26,13 @@ struct lander_t {
 template <typename T> 
 int sgn(T val) { return (T(0) < val) - (val < T(0)); }
 
+template <class T>
+T clip(const T& value, T left, T right) {
+    if (value > right) return right;
+    if (value < left) return left;
+    return value;
+}
+
 std::ostream& operator<<(std::ostream& out, const coords_t& c) {
     return out << "(X: " << c.x << " Y: " << c.y << ") ";
 }
@@ -39,9 +46,11 @@ std::istream& operator>>(std::istream& in, lander_t& lander) {
 
 std::vector<coords_t> surface;
 
+void braking();
 void position_above_landing_zone() ;
 void reduce_horizontal_speed() ;
 void land() ;
+void output(int rotate, int power);
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -66,11 +75,44 @@ int main()
 
 
 
+    //braking();
     position_above_landing_zone();
     reduce_horizontal_speed();
     land();
 
 
+}
+
+void braking() {
+    lander_t lander;
+    double power;
+    int rotate;
+    const int close_distance  = 750;
+
+    do {
+        cin >> lander;
+        cin.ignore();
+        power  =4;
+
+        int speed = std::abs(lander.hSpeed);
+
+        if ( speed > hSpeedThreshold *3) {
+            rotate = 20 * lander.hSpeed/hSpeedThreshold;
+            cerr << "H 4" << endl;
+        } else if ( speed > hSpeedThreshold ) {
+            rotate = 10 * lander.hSpeed/hSpeedThreshold;
+            cerr << "H 3" << endl;
+        } else if ( speed <= hSpeedThreshold) {
+            rotate = 5 * lander.hSpeed/hSpeedThreshold;
+            cerr << "H 2" << endl;
+        } else if ( speed < hSpeedThreshold/4) {
+            rotate = 0;
+            cerr << "H 1" << endl;
+        }
+        cerr << "Braking" << endl;
+        output(rotate, power);
+
+    } while( std::abs(lander.hSpeed) > hSpeedThreshold);
 }
 
 void position_above_landing_zone() {
@@ -84,8 +126,21 @@ void position_above_landing_zone() {
     cerr << "Flat ground: " << flat_ground_start << flat_ground_stop << endl;
     int flat_ground_center_x = (flat_ground_start.x + flat_ground_stop.x) / 2;
     int flat_ground_width = flat_ground_stop.x - flat_ground_start.x;
+    
+    int landing_zone_x_center = flat_ground_center_x;
+    int landing_zone_x_left = flat_ground_start.x;
+    int landing_zone_x_right = flat_ground_stop.x;
 
     cerr << "Center: " << flat_ground_center_x << " Width: " << flat_ground_width << endl;
+
+    int offset = 0 * flat_ground_width;
+
+
+    auto in_landing_zone = [flat_ground_start, flat_ground_stop, offset](coords_t position) {
+        if (position.x < flat_ground_start.x + offset) return -1;
+        if (position.x > flat_ground_stop.x - offset) return 1;
+        return 0;
+    };
 
 
 
@@ -97,48 +152,57 @@ void position_above_landing_zone() {
     do {
         cin >> lander;
         cin.ignore();
-        int landing_zone_x = flat_ground_center_x - 0.5 * flat_ground_width;
-        int distance = landing_zone_x - lander.position.x;
+        int distance = landing_zone_x_center - lander.position.x;
         int distance_abs = std::abs(distance);
         cerr << "Distance: " << distance << endl;
 
-        if (landing_zone_x < lander.position.x) {
+        if (lander.vSpeed > -5) {
+            power = 0;
+        } else if (lander.vSpeed > -7) {
+            power = 1;
+        } else if (lander.vSpeed > -10) {
+            power = 2;
+        } else if (lander.vSpeed > -20) {
+            power = 3;
+        } else {
+            power = 4;
+        }
+
+        if (in_landing_zone(lander.position) == 1) {
             cerr <<"Turning left" << endl;
             rotate=20 ;
-        } else if (landing_zone_x > lander.position.x) {
+        } else if (in_landing_zone(lander.position) == -1) {
             cerr <<"Turning right" << endl;
             rotate=-20;
         } else {
-            rotate = 0;
-        }
-        if (distance_abs < 1000) {
-            rotate *=  distance_abs/1000.0;
-        }
-        if (distance_abs < 1000 && lander.hSpeed < hSpeedThreshold) {
-            rotate = -lander.rotate *  distance_abs/1000.0;
-        }
-        if (sgn(distance) == -sgn(lander.hSpeed) && lander.hSpeed > hSpeedThreshold) {
-            rotate *= lander.hSpeed/hSpeedThreshold * 3.0 / 2.0;
-        }
-
-
-        if (std::abs(distance) < close_distance) {
-            power = 4;
-        }
-
-        if (lander.vSpeed > -5) {
-            power = 0;
-        } else {
-            power = 4;
-        }
-        //power = std::abs(distance)/1000.0 * 4.0;
-        cerr << "Power Distance: " << power << endl;
-        if (sgn(distance) == -sgn(lander.hSpeed) && std::abs(lander.hSpeed) > hSpeedThreshold) {
-            power = 4;
-            cerr << "Power hSpeed: " << power << endl;
+            int speed = std::abs(lander.hSpeed);
+            if ( speed > hSpeedThreshold *3) {
+                rotate = 20 * lander.hSpeed/hSpeedThreshold;
+                cerr << "H 4" << endl;
+            } else if ( speed > hSpeedThreshold ) {
+                rotate = 15 * lander.hSpeed/hSpeedThreshold;
+                power = 5;
+                cerr << "H 3" << endl;
+            } else if ( speed <= hSpeedThreshold) {
+                rotate = 5 * lander.hSpeed/hSpeedThreshold;
+                cerr << "H 2" << endl;
+            } else if ( speed < hSpeedThreshold/2) {
+                rotate = 0;
+                cerr << "H 1" << endl;
+            }
+            //if (distance_abs < 1000) {
+                //rotate *=  distance_abs/1000.0;
+            //}
+            //if (distance_abs < 1000 && std::abs(lander.hSpeed) < hSpeedThreshold) {
+                //rotate = -lander.rotate *  distance_abs/1000.0;
+            //}
+            //if (sgn(distance) == -sgn(lander.hSpeed) && std::abs(lander.hSpeed) > hSpeedThreshold) {
+                //rotate = 10* lander.hSpeed/hSpeedThreshold ;
+            //}
         }
 
-        cout << rotate << " " << int(power>4?4:power) << endl;
+
+        output(rotate, power);
 
     } while( std::abs(lander.hSpeed) > hSpeedThreshold/8
             || lander.position.x < flat_ground_start.x
@@ -151,12 +215,6 @@ void reduce_horizontal_speed() {
 
 }
 
-template <class T>
-T clip(const T& value, T left, T right) {
-    if (value > right) return right;
-    if (value < left) return left;
-    return value;
-}
 
 void land() {
     lander_t lander;
@@ -173,8 +231,13 @@ void land() {
         } else {
             power = 2;
         }
+        output(rotate, power);
 
-        cout << clip(rotate, -90, 90) << " " << clip(int(power), 0, 4) << endl;
 
     } while(1);
 }
+
+void output(int rotate, int power){
+    cout << clip(rotate, -90, 90) << " " << clip(int(power), 0, 4) << endl;
+}
+
